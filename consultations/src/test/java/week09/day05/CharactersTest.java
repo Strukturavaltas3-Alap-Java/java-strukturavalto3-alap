319b567d337dbbc921e2c783987a6e042d8a6d4f
bkris21 <barczaykristof1993@gmail.com>
2022-09-23 15:15:40 +0200

    Week09day05 test for lab

---
+++ consultations/src/test/java/week09/day05/CharactersTest.java
@@ -0,0 +1,22 @@
+package week09.day05;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CharactersTest {
+
+//    Characters characters = new Characters();
+//
+//    @Test
+//    void testFindMostCommonCharacter(){
+//        assertEquals('a',characters.findMostCommonChar("almafa"));
+//        assertEquals('e',characters.findMostCommonChar("Ejj de derék ember ez!"));
+//    }
+//
+//    @Test
+//    void testFindMostCommonCharacterWithEmptyString(){
+//        assertThrows(IllegalArgumentException.class,()-> characters.findMostCommonChar("  "), "String cannot be empty or contain only white spaces!");
+//    }
+
+}
